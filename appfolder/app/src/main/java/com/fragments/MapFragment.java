21fceb1483a36029262ef0815d952bc3cfbcc5e4
personman61 <jeremysl123@gmail.com>
2022-03-29 01:15:50 -0600

    Adds a bunch of stuff, basic nav bar at the top. Orginizes layout of map with nav bar and circular button

--- appfolder/app/src/main/java/com/fragments/MapFragment.java
+++ appfolder/app/src/main/java/com/fragments/MapFragment.java
@@ -2,6 +2,9 @@ package com.fragments;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -22,8 +25,16 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.models.Beacon;
+import com.models.DataCache;
+
+import java.nio.channels.DatagramChannel;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -32,11 +43,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
  */
 public class MapFragment extends Fragment {
 
+    protected LocationManager locationManager;
 
     ImageView radarPulse;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         //Initialize view
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
@@ -54,6 +68,17 @@ public class MapFragment extends Fragment {
             @Override
             public void onMapReady(@NonNull GoogleMap googleMap) {
                 //When map is loaded
+                DataCache cache = DataCache.getInstance();
+                cache.locationData.setGoogleMap(googleMap);
+                List<Beacon> beaconList = cache.getBeaconList();
+                for (Beacon beacon: beaconList) {
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(new LatLng(beacon.latitude, beacon.longitude));
+                    markerOptions.title(beacon.title);
+                    markerOptions.snippet(beacon.getDescription());
+                    googleMap.addMarker(markerOptions);
+                }
+
                 googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
                     @Override
                     public void onMapClick(@NonNull LatLng latLng) {
@@ -64,14 +89,9 @@ public class MapFragment extends Fragment {
                         markerOptions.position(latLng);
                         //Set title of marker
                         markerOptions.title(latLng.latitude + " : " + latLng.longitude);
-                        //Remove all marker
-                        googleMap.clear();
-                        //Animating to zoom of marker
-                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                                latLng, 10
-                        ));
+
+
                         //Add marker on Map
-                        googleMap.addMarker(markerOptions);
                     }
                 });
             }
@@ -82,6 +102,8 @@ public class MapFragment extends Fragment {
         //Return View
         return view;
     }
+
+
 
     private void rotate(ImageView view, Context context) {
 
